4be078bd23f7d56f63268498a69c9d5f32a00e6e
vinhvdt <thienvinhvpv@gmail.com>
2022-11-10 15:27:39 +0700

    string python

---
+++ string/py1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 15:06:40 2022
+
+@author: V
+
+Description:
+    Write a Python programe that prints the length of a string s.
+    
+Expected Output:
+    Input: "", "H", "Hello", "Amazing"
+    Output: 0, 1, 5, 7
+"""
+# 1. Challenge Print the Length of a String
+print('Solution Print the Length of a String')
+s = ''
+s1 = 'H'
+s2 = 'Hello'
+s3 = 'Amazing'
+print('Length of the {} is:'.format(s), len(s))
+print('Length of the {} is:'.format(s1), len(s1))
+print('Length of the {} is:'.format(s2), len(s2))
+print('Length of the {} is:'.format(s3), len(s3))
+
